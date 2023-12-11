49f423298996272c3a8d9df7c4ede3a7b8d7cf7f
Sofia Gaisina <129632311+ssupersonicc@users.noreply.github.com>
2023-12-11 12:10:24 +0300

    Update syntax_analyzer.h

--- syntax_analyzer.h
+++ syntax_analyzer.h
@@ -1,33 +1,117 @@
 #pragma once
-#include <vector>
+#include <SyntaxError.h>
+
+#include <deque>
 #include <string>
+#include <vector>
+
 #include "lexical_analyzer.h"
 
 void ReadFile(std::string file, std::vector<char> &text);
-void Program();
-Lexem request_lexem();
 
-void Assignment();
+const std::vector<std::string> TypeList = {"int", "void", "bool", "double"};
+
+// Get lexems
+
+void GetLexem();
+
+// Program Structure
+
+void Program();
+void Directive();
+
+// Functions
+
+void FunctionDefinition();
+void FunctionParameters();
+void FunctionCall();
+void ArgumentList();
+
+// Structures
+
+void StructDefinition();
+void StructMember();
+
+// Types and variables
+
+void String();
+void Symbol(char symbol);
+void Name();
 void Variable();
-void Assignment_Operator();
+bool Letter(char c);
+bool SpecialSymbol(char c);
+bool Digit(char c);
+bool Type();
+void VariableCreation();
+
+// void VariableDefinition();
+//
+// void VariableInitialization();
+
+bool Sign();
+bool SignedNumber();
+bool UnsignedNumber();
+
+// Expression
+
 void Expression();
-void Comparison();
-void Comparisons_Operators();
+void ExpressionTerm();
+
+// Arithmetic, logic, comparison and assignment
+
+void ArithmeticExpression();
+bool Unary();
+bool Mul();
+bool Sum();
+bool Power();
+bool And();
+bool Xor();
+bool Or();
+bool Shift();
+bool Equality();
+bool NonEquality();
+bool Assignment();
+void UnaryTerm();
+void PowerTerm();
+void MulTerm();
+void SumTerm();
+void ShiftTerm();
+void NonEqualityTerm();
+void EqualityTerm();
+void AndTerm();
+void XorTerm();
+void OrTerm();
+void AssignmentTerm();
+void ArithmeticTerm();
+bool ArithmeticLiteral();
+bool BooleanLiteral();
+
+// Operator
+
 void Operator();
+
+// Conditional operator
+
 void If();
+
+// Cycles
+
 void For();
 void While();
+
+// Input and Output
+
 void Input();
 void Output();
-void Variable_Creation();
-void Match();
-void Variable_Initialization();
-void Name();
-void Array_Declaration();
-void Array_Declaration_Exact();
-void Array_Declaration_Auto();
-void Type();
+
+// Arrays
+
+void ArrayIndexation();
+void ArrayDeclaration();
+void ArrayDeclarationAuto();
+void ArrayDeclarationExact();
 void Literal();
-void Array_Indexation();
-void Arithmetic_Literal();
-void Boolean_Literal();
+
+// Match
+
+void Match();
