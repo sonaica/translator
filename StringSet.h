1f680a8d9b462b62eee247ae05d2f7d45d3951e1
SergeyGrigoryan <107277137+sir-greg@users.noreply.github.com>
2023-12-25 15:05:19 +0300

    Create StringSet.h

---
+++ StringSet.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <vector>
+#include <string>
+
+template<class T>
+class StringSet {
+    
+    struct Vertex {
+
+        const int ALPHABET_SIZE = 1 << 8;
+        
+        int move_to[ALPHABET_SIZE];
+        bool terminal;
+        T extra_value;
+
+        Vertex(T value = T());
+
+        bool terminate();
+    };
+
+    std::vector<Vertex> vertex_list;
+
+    int add_vertex();
+
+public:
+
+    StringSet();
+
+    void insert(const std::string& str, T value = T());
+
+    std::pair<bool, T> find(const std::string& str);
+};
