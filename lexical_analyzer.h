23228a311f6907c29176517257cb140ec220ce94
SergeyGrigoryan <107277137+sir-greg@users.noreply.github.com>
2023-12-25 15:04:02 +0300

    Update lexical_analyzer.h

--- lexical_analyzer.h
+++ lexical_analyzer.h
@@ -13,7 +13,7 @@ const int string_literal_type = 6;
 const int operation_type = 7;
 
 struct Vertex {
-    Vertex *to[256] = {0};
+    Vertex* to[256] = { 0 };
     bool terminal = false;
 };
 
@@ -42,7 +42,7 @@ void CreateBor(std::string file_keywords);
 bool is_operation(char c);
 bool is_letter(char c);
 bool is_digit(char c);
-bool is_keyword(std::vector<char> &text, int &i, std::string &keyword);
+bool is_keyword(std::vector<char>& text, int& i, std::string& keyword);
 bool is_alphabet(char c);
 Lexem create_lexem(std::string str, int type);
 Verdict FSM();
