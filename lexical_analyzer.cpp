7a194280b11bcd1ccb57561864eccea871f626a5
SergeyGrigoryan <107277137+sir-greg@users.noreply.github.com>
2023-12-25 15:03:53 +0300

    Update lexical_analyzer.cpp

--- lexical_analyzer.cpp
+++ lexical_analyzer.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <vector>
 
-Vertex *root = new Vertex();
+Vertex* root = new Vertex();
 
 extern std::vector<char> text;
 extern int pos;
@@ -30,7 +30,7 @@ void CreateBor(std::string file_keywords) {
 bool is_operation(char c) {
     if (c == '=' || c == '+' || c == '-' || c == '~' || c == '&' || c == '|' ||
         c == '*' || c == '>' || c == '<' || c == '/' || c == '^' || c == '%' ||
-        c == '.')
+        c == '.' || c == '!')
         return true;
     return false;
 }
@@ -77,6 +77,7 @@ void update_line(bool f) {
 }
 
 Verdict FSM() {
+    bool point = false;
     Lexem lex;
     States state = States::H;
     char cur;
@@ -85,142 +86,180 @@ Verdict FSM() {
     while (pos < text.size()) {
         cur = text[pos];
         switch (state) {
-            case States::H:
-                if (cur != ' ') update_line(false);
-                if (cur == '/' && pos + 1 < text.size() &&
-                    text[pos + 1] == '/') {
-                    pos += 2;
-                    state = States::Comment;
-                } else if (is_letter(cur)) {
+        case States::H:
+            point = false;
+            if (cur != ' ') update_line(false);
+            if (cur == '/' && pos + 1 < text.size() &&
+                text[pos + 1] == '/') {
+                pos += 2;
+                state = States::Comment;
+            }
+            else if (is_letter(cur)) {
+                current_lexem += cur;
+                ++pos;
+                state = States::Id;
+            }
+            else if (is_digit(cur)) {
+                current_lexem += cur;
+                ++pos;
+                state = States::Literal_Int;
+            }
+            else if (is_operation(cur)) {
+                current_lexem += cur;
+                ++pos;
+                state = States::Operation;
+            }
+            else if (cur == ',') {
+                current_lexem += cur;
+                ++pos;
+                verdict_return.lexem = create_lexem(current_lexem, 9);
+                return verdict_return;
+            }
+            else if (cur == '(' || cur == ')') {
+                current_lexem += cur;
+                ++pos;
+                verdict_return.lexem = create_lexem(current_lexem, 10);
+                return verdict_return;
+            }
+            else if (cur == '[' || cur == ']') {
+                current_lexem += cur;
+                ++pos;
+                verdict_return.lexem = create_lexem(current_lexem, 11);
+                return verdict_return;
+            }
+            else if (cur == '{' || cur == '}' || cur == ';') {
+                current_lexem += cur;
+                ++pos;
+                verdict_return.lexem = create_lexem(current_lexem, 8);
+                return verdict_return;
+            }
+            else if (cur == '\n') {
+                ++lines;
+                ++pos;
+                update_line(true);
+            }
+            else if (cur == ' ')
+                ++pos;
+            else if (cur == '\"') {
+                current_lexem += cur;
+                ++pos;
+                state = States::Literal_String;
+            }
+            else if (!is_alphabet(cur)) {
+                verdict_return.is_error = true;
+                verdict_return.type = 1;
+                return verdict_return;
+            }
+            else {
+                verdict_return.is_error = true;
+                verdict_return.type = 2;
+                return verdict_return;
+            }
+            break;
+        case States::Id:
+            if (is_letter(cur) || is_digit(cur)) {
+                current_lexem += cur;
+                ++pos;
+            }
+            else if (cur == '.') {
+                if (!point) {
                     current_lexem += cur;
                     ++pos;
-                    state = States::Id;
-                } else if (is_digit(cur)) {
-                    current_lexem += cur;
-                    ++pos;
-                    state = States::Literal_Int;
-                } else if (is_operation(cur)) {
-                    current_lexem += cur;
-                    ++pos;
-                    state = States::Operation;
-                } else if (cur == ',') {
-                    current_lexem += cur;
-                    ++pos;
-                    verdict_return.lexem = create_lexem(current_lexem, 9);
-                    return verdict_return;
-                } else if (cur == '(' || cur == ')') {
-                    current_lexem += cur;
-                    ++pos;
-                    verdict_return.lexem = create_lexem(current_lexem, 10);
-                    return verdict_return;
-                } else if (cur == '[' || cur == ']') {
-                    current_lexem += cur;
-                    ++pos;
-                    verdict_return.lexem = create_lexem(current_lexem, 11);
-                    return verdict_return;
-                } else if (cur == '{' || cur == '}' || cur == ';') {
-                    current_lexem += cur;
-                    ++pos;
-                    verdict_return.lexem = create_lexem(current_lexem, 8);
-                    return verdict_return;
-                } else if (cur == '\n') {
-                    ++lines;
-                    ++pos;
-                    update_line(true);
-                } else if (cur == ' ')
-                    ++pos;
-                else if (cur == '\"') {
-                    current_lexem += cur;
-                    ++pos;
-                    state = States::Literal_String;
-                } else if (!is_alphabet(cur)) {
+                    point = true;
+                }
+                else {
                     verdict_return.is_error = true;
-                    verdict_return.type = 1;
-                    return verdict_return;
-                } else {
-                    verdict_return.is_error = true;
-                    verdict_return.type = 2;
+                    verdict_return.type = 3;
                     return verdict_return;
                 }
-                break;
-            case States::Id:
-                if (is_letter(cur) || is_digit(cur)) {
-                    current_lexem += cur;
-                    ++pos;
-                } else {
-                    if (is_keyword(current_lexem)) {
-                        if (current_lexem == "true" ||
-                            current_lexem == "false") {
-                            verdict_return.lexem =
-                                create_lexem(current_lexem, literal_bool_type);
-                            return verdict_return;
-                        } else {
-                            verdict_return.lexem =
-                                create_lexem(current_lexem, keyword_type);
-                            return verdict_return;
-                        }
+            }
+            else {
+                if (is_keyword(current_lexem)) {
+                    if (current_lexem == "true" ||
+                        current_lexem == "false") {
+                        verdict_return.lexem =
+                            create_lexem(current_lexem, literal_bool_type);
+                        return verdict_return;
                     }
-                    verdict_return.lexem =
-                        create_lexem(current_lexem, identifier_type);
-                    return verdict_return;
+                    else if (current_lexem == "and" ||
+                        current_lexem == "or") {
+                        verdict_return.lexem =
+                            create_lexem(current_lexem, operation_type);
+                        return verdict_return;
+                    }
+                    else {
+                        verdict_return.lexem =
+                            create_lexem(current_lexem, keyword_type);
+                        return verdict_return;
+                    }
                 }
-                break;
-            case States::Operation:
-                if (is_operation(cur)) {
-                    current_lexem += cur;
-                    ++pos;
-                } else {
-                    verdict_return.lexem =
-                        create_lexem(current_lexem, operation_type);
-                    return verdict_return;
-                }
-                break;
-            case States::Literal_Int:
-                if (is_digit(cur)) {
-                    current_lexem += cur;
-                    ++pos;
-                } else if (cur == '.') {
-                    current_lexem += cur;
-                    ++pos;
-                    state = States::Literal_Double;
-                } else if (is_letter(cur)) {
-                    verdict_return.is_error = true;
-                    verdict_return.type = 1;
-                    return verdict_return;
-                } else {
-                    verdict_return.lexem =
-                        create_lexem(current_lexem, literal_int_type);
-                    return verdict_return;
-                }
-                break;
-            case States::Literal_Double:
-                if (is_digit(cur)) {
-                    current_lexem += cur;
-                    ++pos;
-                } else {
-                    verdict_return.lexem =
-                        create_lexem(current_lexem, literal_double_type);
-                    return verdict_return;
-                }
-                break;
-            case States::Comment:
-                if (cur != '\n')
-                    ++pos;
-                else
-                    state = States::H;
-                break;
-            case States::Literal_String:
-                if (cur != '\"') {
-                    current_lexem += cur;
-                    ++pos;
-                } else {
-                    current_lexem += cur;
-                    ++pos;
-                    verdict_return.lexem =
-                        create_lexem(current_lexem, string_literal_type);
-                    return verdict_return;
-                }
-                break;
+                verdict_return.lexem =
+                    create_lexem(current_lexem, identifier_type);
+                return verdict_return;
+            }
+            break;
+        case States::Operation:
+            if (is_operation(cur)) {
+                current_lexem += cur;
+                ++pos;
+            }
+            else {
+                verdict_return.lexem =
+                    create_lexem(current_lexem, operation_type);
+                return verdict_return;
+            }
+            break;
+        case States::Literal_Int:
+            if (is_digit(cur)) {
+                current_lexem += cur;
+                ++pos;
+            }
+            else if (cur == '.') {
+                current_lexem += cur;
+                ++pos;
+                state = States::Literal_Double;
+            }
+            else if (is_letter(cur)) {
+                verdict_return.is_error = true;
+                verdict_return.type = 1;
+                return verdict_return;
+            }
+            else {
+                verdict_return.lexem =
+                    create_lexem(current_lexem, literal_int_type);
+                return verdict_return;
+            }
+            break;
+        case States::Literal_Double:
+            if (is_digit(cur)) {
+                current_lexem += cur;
+                ++pos;
+            }
+            else {
+                verdict_return.lexem =
+                    create_lexem(current_lexem, literal_double_type);
+                return verdict_return;
+            }
+            break;
+        case States::Comment:
+            if (cur != '\n')
+                ++pos;
+            else
+                state = States::H;
+            break;
+        case States::Literal_String:
+            if (cur != '\"') {
+                current_lexem += cur;
+                ++pos;
+            }
+            else {
+                current_lexem += cur;
+                ++pos;
+                verdict_return.lexem =
+                    create_lexem(current_lexem, string_literal_type);
+                return verdict_return;
+            }
+            break;
         }
     }
     return verdict_return;
